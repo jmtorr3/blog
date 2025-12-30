5fbe9e87bca383551c864dc6d0ba4480f64a44e0
slumpy666 <dr.slumpy.666@gmail.com>
2025-12-29 20:59:11 -0500

    fixed redirect after token expiration

--- frontend/src/components/Layout/ProtectedRoute.jsx
+++ frontend/src/components/Layout/ProtectedRoute.jsx
@@ -9,7 +9,7 @@ function ProtectedRoute({ children }) {
   }
 
   if (!user) {
-    return <Navigate to="/login" replace />;
+    return <Navigate to="/blog/login" replace />;
   }
 
   return children;
